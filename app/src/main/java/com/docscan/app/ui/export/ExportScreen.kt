922382f1f4d9a9c8fcb30a6352ca42a6d150a9db
Ankit Rajput <ankitoffical24@gmail.com>
2025-12-31 17:23:01 +0530

    unresolved clip error in cropeditorscreen.kt resolved and in exportsScreen.kt stringresource function is fixed

--- app/src/main/java/com/docscan/app/ui/export/ExportScreen.kt
+++ app/src/main/java/com/docscan/app/ui/export/ExportScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
